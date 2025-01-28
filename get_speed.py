4121acb544c2bdd32e808e48d30e3ff66f079aae
Wintzbi <leynaickd@gmail.com>
2025-01-28 09:20:52 +0100

    isolation of the speed attribute

--- get_speed.py
+++ get_speed.py
@@ -1,4 +1,5 @@
 from zwift import Client
+import time
 
 username = "leynaickd@gmail.com"
 password = "@Ymdthw62"
@@ -11,8 +12,10 @@ print(f"World ID: {world.world_id}")
 profile = client.get_profile()
 print(f"Player ID: {profile.player_id}")
 
-player_status = world.player_status(player_id)
+while world.world_id == 1:
+    player_status = world.player_status(player_id)
+    print("Vitesse actuelle du joueur :", player_status.player_state.__getattribute__("speed")*0.00001)
+    time.sleep(1)
 
-print(f"Vitesse actuelle du joueur : {player_status.player_state["speed"]}")
 
 
